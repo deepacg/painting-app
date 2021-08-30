4c00d4f7305fd073bf2af2ddff229bd46ace8e0d
Deepa George <54993216+deepacg@users.noreply.github.com>
2021-08-30 19:32:57 +0530

    touch update2

--- painting.js
+++ painting.js
@@ -21,11 +21,21 @@ let painting = false        // to record mouse click
 function paintingStart(e) {     // mouse down
     painting = true
     draw(e)
+    
+}   
+
+function paintingTouchStart(e){
+    painting = true
     if (e.target.nodeName == 'CANVAS') { 
         e.preventDefault(); 
     } 
-}   
+}
 function paintingEnd(e){        // mouse up
+    painting = false
+    ctx.beginPath()
+}
+
+function paintingTouchEnd(e){
     painting = false
     ctx.beginPath()
     if (e.target.nodeName == 'CANVAS') { 
@@ -34,9 +44,6 @@ function paintingEnd(e){        // mouse up
 }
 function draw(e){
     if(!painting) return
-    if (e.target.nodeName == 'CANVAS') { 
-        e.preventDefault(); 
-    } 
 
     let x = e.clientX
     let y = e.clientY - canvas.offsetTop
@@ -50,13 +57,31 @@ function draw(e){
     ctx.strokeStyle = brushColor
     ctx.stroke()
 }
+function drawTouch(e){
+    if(!painting) return
+    if (e.target.nodeName == 'CANVAS') { 
+        e.preventDefault(); 
+    } 
+
+    let x = e.touches[0].pageX
+    let y = e.touches[0].pageY
+
+    ctx.lineWidth = brushSize
+    ctx.lineCap = 'round'
+
+    // ctx.beginPath()
+    // ctx.moveTo(x, y)
+    ctx.lineTo(x, y)
+    ctx.strokeStyle = brushColor
+    ctx.stroke()
+}
 
 canvas.addEventListener('mousedown', paintingStart)
 canvas.addEventListener('mouseup', paintingEnd)
 canvas.addEventListener('mousemove', draw)
-canvas.addEventListener('touchstart', paintingStart, false)
-canvas.addEventListener('touchend', paintingEnd, false)
-canvas.addEventListener('touchmove', draw, false)
+canvas.addEventListener('touchstart', paintingTouchStart, false)
+canvas.addEventListener('touchend', paintingTouchEnd, false)
+canvas.addEventListener('touchmove', drawTouch, false)
 
 
 
