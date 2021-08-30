7ab68828df46338cf62c54ae71cc7a59012dee71
Deepa George <54993216+deepacg@users.noreply.github.com>
2021-08-30 19:42:02 +0530

    added offset

--- painting.js
+++ painting.js
@@ -43,10 +43,10 @@ function draw(e){
     if(e.type == 'touchstart' || e.type == 'touchmove' || e.type == 'touchend' || e.type == 'touchcancel'){
         var touch = e.originalEvent.touches[0] || e.originalEvent.changedTouches[0];
         x = touch.pageX;
-        y = touch.pageY;
+        y = touch.pageY - canvas.offsetTop;
     } else if (e.type == 'mousedown' || e.type == 'mouseup' || e.type == 'mousemove' || e.type == 'mouseover'|| e.type=='mouseout' || e.type=='mouseenter' || e.type=='mouseleave') {
         x = e.clientX;
-        y = e.clientY;
+        y = e.clientY - canvas.offsetTop;
     }
     // let x = e.clientX
     // let y = e.clientY - canvas.offsetTop
